0f7b4c34b16f0199ba4f92e19f5a7f05f1db1564
bharath <bharathprasannan96@gmail.com>
2023-07-18 18:28:35 +0530

    init

---
+++ webScript.js
@@ -0,0 +1,18 @@
+
+ 
+
+// var start = () =>
+// console.log(pc)
+
+//   navigator.mediaDevices
+//     .getUserMedia({ audio: true, video: true })
+//     .then((stream) => {
+//       attachVideo(v1, v1info, stream);
+//     })
+//     .catch(console.error);
+
+//     var dimensions = v => v.videoWidth +"x"+ v.videoHeight;
+//     var attachVideo = (videoTag, vinfo, stream) => {
+//         videoTag.srcObject = stream;
+//         videoTag.addEventListener("loadedmetadata", e => update(vinfo, dimensions(videoTag)), false );
+//       };
